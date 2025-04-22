1219bafed5730a71d2b7ea338ce1de27369a131f
Laboratorio C205 <labc205@uam.net.ni>
2025-04-22 11:21:07 -0600

    ejercicio 1 en python

---
+++ actividad4/ejercicio1.py
@@ -0,0 +1,9 @@
+inicio = int(input("Dime el valor inicial: "))
+fin = int(input("Dime el valor final: "))
+
+numero = int(input("Dime un número: "))
+
+if numero >= inicio and numero <= fin:
+    print("El número es válido")
+else:
+    print("El número no es válido")
