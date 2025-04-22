0d62fc39cf4fee84004481885a3737587245fcf6
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-22 07:51:13 -0600

    se realizo un formato a la salida

--- Semana 1/actividad3/ejercicio1.py
+++ Semana 1/actividad3/ejercicio1.py
@@ -1,1 +1,10 @@
-print("Hola")
+calificacion1 = int(input("Ingrese la calificacion 1: "))
+calificacion2 = int(input("Ingrese la calificacion 2: "))
+calificacion3 = int(input("Ingrese la calificacion 3: "))
+suma = calificacion1 + calificacion2 + calificacion3
+promedio = suma / 3 
+
+print(f"""calificación 1: {calificacion1:>3}
+calificación 2: {calificacion2:>3}
+Calificación 3: {calificacion3:>3}
+{"Promedio:":<15} {promedio:>3.0f}""")
