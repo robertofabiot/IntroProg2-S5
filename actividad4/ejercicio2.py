cc6dd1c8e430ed031a5b5dbe2acccf18837d1a14
Roberto Fabio Tercero Membreño <rftercerom@uamv.edu.ni>
2025-04-23 20:59:05 -0600

    Corrección ejercicio 2

--- actividad4/ejercicio2.py
+++ actividad4/ejercicio2.py
@@ -1,7 +1,7 @@
 import datetime as dt
-fecha_ingreso = input("Fecha ingreso YYYY-MM-DD")
-fecha_ingreso = dt.datetime.strptime(fecha_ingreso"%Y-%m-%d")
-fecha_actual = dt.datetime.now
+fecha_ingreso = input("Digite la fecha de ingreso en formato YYYY-MM-DD: ")
+fecha_ingreso = dt.datetime.strptime(fecha_ingreso,"%Y-%m-%d")
+fecha_actual = dt.datetime.now()
 
 dias = (fecha_actual - fecha_ingreso).days
 print(fecha_actual)
