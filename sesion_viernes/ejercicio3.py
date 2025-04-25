299f226cb21cd31f495949e52be532aebdf4c4d5
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-25 08:13:15 -0600

    Ejercicio 3

---
+++ sesion_viernes/ejercicio3.py
@@ -0,0 +1,18 @@
+dia = int(input("Ingresa el día de la semana de forma numérica (1-7): "))
+
+if dia == 1:
+    print("Lunes")
+elif dia == 2:
+    print("Martes")
+elif dia == 3:
+    print("Miércoles")
+elif dia == 4:
+    print("Jueves")
+elif dia == 5:
+    print("Viernes")
+elif dia == 6:
+    print("Sábado")
+elif dia == 7:
+    print("Domingo")
+else:
+    print("Número inválido. Debe ser entre 1 y 7.")
