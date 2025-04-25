d59a7ef7979d37b32872c92ac639be67e9276bc5
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-25 07:35:07 -0600

    Ejercicio 1

---
+++ sesion_viernes/ejercicio1.py
@@ -0,0 +1,14 @@
+calificacion = float(input("Ingrese la calificación del estudiante (0-10): "))
+
+if calificacion >= 9 and calificacion <=10:
+    print("La calificación es A")
+elif calificacion >= 7 and calificacion <=8:
+    print("La calificación es A")
+elif calificacion >= 5 and calificacion <=6:
+    print("La calificación es A")
+elif calificacion >= 3 and calificacion <=4:
+    print("La calificación es A")
+elif calificacion >= 1 and calificacion <=2:
+    print("La calificación es A")
+else:
+    print("La calificación no es válida")
